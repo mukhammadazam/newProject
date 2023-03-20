73fa5f9e79a2868c070333021b04c77e84ee8241
mardon1dev <dusbekovmardonbek5430@gmail.com>
2023-03-20 23:31:05 +0500

    we

--- newproject/src/App.js
+++ newproject/src/App.js
@@ -1,19 +1,11 @@
 
 import Header from './Home/Header/Header';
 import './Home/scss/main.scss';
-<<<<<<< HEAD
-import Section from './Home/Section';
-function App() {
-  return (
-    <div className="App">
-      <Section />
-=======
 
 function App() {
   return (
     <div className="App">
       <Header/>
->>>>>>> 69e7ce81867dbb0a6a302e814660c8ff441fa351
     </div>
   );
 }
