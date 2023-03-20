f754f26f5f4d481db7c5875825f65adde537231b
mardon1dev <dusbekovmardonbek5430@gmail.com>
2023-03-20 23:53:35 +0500

    cdcq

--- newproject/src/Home/Header/Header.jsx
+++ newproject/src/Home/Header/Header.jsx
@@ -1,10 +1,17 @@
 import '../scss/main.scss';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Logo from './logo.svg';
+
 
 const Header = () => {
   return (
-    <>
+    <Navbar bg="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home"><img src={Logo} alt="Logo" /></Navbar.Brand>
         
-    </>
+      </Container>
+    </Navbar>
   )
 }
 
