6d7cc4487c0cafc1762fea74df8262a329a85bc9
mardon1dev <dusbekovmardonbek5430@gmail.com>
2023-03-21 03:58:34 +0500

    vdve

--- newproject/src/App.js
+++ newproject/src/App.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-
-import Header from './Home/Header/Header';
-// import Section from './Home/Section';
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      {/* <Section /> */}
-=======
 import './Home/scss/main.scss'
 import Section from './Home/Section';
 import Tovar from './Home/Tovar';
@@ -16,7 +6,6 @@ function App() {
     <div className="App">
       <Section />
       <Tovar />
->>>>>>> 38a653eb7d5a643e2c16794946519bc5ca2727e9
     </div>
   );
 }
