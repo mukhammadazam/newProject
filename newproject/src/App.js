778ba19e3d09df07b5163ee6fae1ab07905883da
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-03-21 12:15:43 +0300

    News finished

--- newproject/src/App.js
+++ newproject/src/App.js
@@ -3,13 +3,17 @@ import Section from './Home/Section';
 import Tovar from './Home/Tovar';
 import Header from './Home/Header/Header';
 import Hero from './Home/Hero/Hero';
+import Comunecat from './Home/Comunecat';
+import News from './Home/News';
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Hero/>
+      <Header />
+      <Hero />
       <Section />
       <Tovar />
+      <Comunecat />
+      <News />
     </div>
   );
 }
---
+++ newproject/src/Home/Comunecat.jsx
@@ -0,0 +1,16 @@
+
+
+const Comunecat = () => {
+   
+  return (
+    <div className="container">
+      <div className="new">
+        <div className="new__enner  d-lg-flex justify-content-center align-items-lg-center justify-content-lg-between">
+          <h4 className="new__enner--title pb-5 text-center">Новые статьи</h4>
+          <button className="new__enner--btn d-block">Все статьи</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Comunecat;
